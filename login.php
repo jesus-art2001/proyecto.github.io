26cb4a6fe467a3ab82d3dbc3b78144d48cee0d74
jesus-art2001 <67523687+jesus-art2001@users.noreply.github.com>
2020-08-12 13:53:14 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina empresarial</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+   
+    <article>
+      <h1>formulario login</h1>
+       
+    </article>
+
+    
+    <section>
+        
+    </section>
+
+    <footer>
+        
+    </footer>
+
+</body>
+</html>
